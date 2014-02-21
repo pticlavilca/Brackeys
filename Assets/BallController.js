f8f32cfa6cc3ea1adf03d70268908a3fee82a060
pticlavilca <pticlavilca@gmail.com>
2014-02-21 17:31:01 -0500

    add files

---
+++ Assets/BallController.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var ROTATION_SPEED = 100;
+
+
+
+function Update () {
+
+
+	var rotation: float = Input.GetAxis("Horizontal") * ROTATION_SPEED;
+	rotation *= Time.deltaTime;
+	rigidbody.AddRelativeTorque (Vector3.back * rotation);
+
+}
