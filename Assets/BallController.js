32453cf9419210788abbc166de29f280d09ce102
pticlavilca <pticlavilca@gmail.com>
2014-02-21 18:02:18 -0500

    add function jumping

--- Assets/BallController.js
+++ Assets/BallController.js
@@ -1,8 +1,9 @@
 ﻿#pragma strict
 
 var ROTATION_SPEED = 100;
+var JUMP_HEIGHT = 8;
 
-
+private var isFalling = false;
 
 function Update () {
 
@@ -10,5 +11,16 @@ function Update () {
 	var rotation: float = Input.GetAxis("Horizontal") * ROTATION_SPEED;
 	rotation *= Time.deltaTime;
 	rigidbody.AddRelativeTorque (Vector3.back * rotation);
+	
+	if(Input.GetKeyDown(KeyCode.W) && isFalling == false){
+		
+		rigidbody.velocity.y = JUMP_HEIGHT;
+		
+	}
+	isFalling = true;
+
+}
+function OnCollisionStay(){
+	isFalling = false;
 
 }
